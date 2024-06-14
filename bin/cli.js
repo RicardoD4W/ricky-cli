59be097a53e2538b076e1fae8fdae52f0c17979b
ricardo <ricardodeveloper03@gmail.com>
2024-06-14 15:09:20 +0200

    notify

--- bin/cli.js
+++ bin/cli.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node
 import { main } from "../src/index.js";
 import updateNotifier from "update-notifier";
-import pkg from "../package.json" assert { type: "json" };
+import packageJson from "../package.json" assert { type: "json" };
 
-const notifier = updateNotifier({ pkg });
-if (notifier.update) {
-  notifier.notify();
-}
+updateNotifier({ pkg: packageJson }).notify();
 
 main();
