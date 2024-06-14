4bd87741f7b94919843c9ff6bbcf75eab18ce06b
ricardo <ricardodeveloper03@gmail.com>
2024-06-14 14:38:21 +0200

    aviso de nueva versión

--- bin/cli.js
+++ bin/cli.js
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
 import { main } from "../src/index.js";
+import updateNotifier from "update-notifier";
+import pkg from "../package.json" assert { type: "json" };
+
+const notifier = updateNotifier({ pkg });
+if (notifier.update) {
+  notifier.notify();
+}
 
 main();
