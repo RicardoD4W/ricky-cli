65bb8bb5301ed7353eb24932742fb4c24facf74a
ricardo <ricardodeveloper03@gmail.com>
2024-08-14 00:10:30 +0200

    delete \n in react view

--- src/templates/component/react/component.view.tsx
+++ src/templates/component/react/component.view.tsx
@@ -2,7 +2,6 @@ import {COMPONENT_NAME}Theme from './css/{camelCaseName}.module.css';
 import { type {COMPONENT_NAME}Model } from './{camelCaseName}.model';
 import { use{COMPONENT_NAME}ViewModel } from "./{camelCaseName}.viewmodel";
 
-
 export default function {COMPONENT_NAME}View (props : {COMPONENT_NAME}Model){
   const { {camelCaseName}Title } = use{COMPONENT_NAME}ViewModel(props);
 
