ee0c66ba00e8a94f93b26a8f6ae65a44089f35c5
ricardo <ricardodeveloper03@gmail.com>
2025-03-03 10:50:14 +0100

    correccion de versiones de node

--- bin/cli.js
+++ bin/cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { main } from "../src/index.js";
 import updateNotifier from "update-notifier";
-import packageJson from "../package.json" assert { type: "json" };
+import packageJson from "../package.json" with { type: "json" };
 import { welcomeMsg } from "../src/views/welcome.js";
 import { versionMsg } from "../src/views/version.js";
 
